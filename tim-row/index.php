74b89b823ab31980b2d4f23d72e15a8850d0df2e
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2021-02-26 15:24:30 +0800

    Rev Index

--- tim-row/index.php
+++ tim-row/index.php
@@ -28,7 +28,13 @@ require('template/header.php');
                   <div class="item">
                     <h3><span class="text-custom font-600">Hey! Selamat Datang</span></h3>
                     <p class="small"><?= date('d F, Y') ?></p>
-                    <p class="m-t-30"><em>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</em></p>
+                    <p class="m-t-30"><em>
+                      PT PLN (Persero) Unit Pelaksana Transmisi Makassar (UPT Makassar) berdiri berdasarkan keputusan direksi Nomor 1789.P/DIR/2018 tanggal 19 Desember 2018 setelah sebelumnya bernama Unit Pelayanan Transmisi Makassar.
+                      <br>
+                      Tugas pokok PLN UPT Makassar adalah merencanakan, melaksanakan dan melakukan evaluasi serta membuat laporan atas kegiatan operasional penyaluran tenaga listrik dan pemeliharaan jaringan transmisi dan gardu induk secara efisien dengan mutu dan keandalan yang baik.
+                      <br>
+                      PLN UPT Makassar membawahi 7 Unit Layanan Transmisi dan Gardu Induk (ULTG) dan 46 Gardu Induk yang tersebar di dua provinsi yakni Provinsi Sulawesi Selatan dan Sulawesi Tenggara. Aset PLN UPT Makassar saat ini untuk panjang transmisi sebesar 3055.61 kms dan kapasitas trafo sebesar 2473.3 MVA
+                    </em></p>
                   </div>
                 </div>
               </div>
